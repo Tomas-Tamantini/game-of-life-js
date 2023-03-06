92d7c2c23a3528c3ac6bbfc457a8fe25bcf36efd
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-03-06 13:24:08 -0300

    Add paly/pause button

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ import { simpleGliderCells, bigGliderCells } from "./models/samples.js";
 
 const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
+const toggleBtn = document.getElementById("toggle-animation-btn");
 const cellSize = 6;
 let frameCount = 0;
 let framesPerSecond = 10;
+let isPaused = false;
 
 const game = new GameOfLife(bigGliderCells);
+
+toggleBtn.addEventListener("click", () => {
+  isPaused = !isPaused;
+});
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -20,6 +26,7 @@ function draw() {
 function animate() {
   requestAnimationFrame(animate);
   draw();
+  if (isPaused) return;
   frameCount++;
   if (frameCount % Math.floor(60 / framesPerSecond) === 0) game.step();
 }
