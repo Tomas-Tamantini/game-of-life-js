930f4cb83eaaa5cc3adc4228758d2fe65c8c72c7
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-03-06 11:35:51 -0300

    Test cells that remain dead

--- tests/gameOfLifeTestCases.js
+++ tests/gameOfLifeTestCases.js
@@ -4,6 +4,21 @@ function assert(assertion) {
   if (assertion === false) throw Error("Test failed");
 }
 
+function generateNeighbors(cellCoords, numNeighbors) {
+  const [x, y] = cellCoords;
+  let neighbors = [];
+  let count = 0;
+  for (let i = -1; i <= +1; i++) {
+    for (let j = -1; j <= +1; j++) {
+      if (i === 0 && j === 0) continue;
+      if (count === numNeighbors) return neighbors;
+      neighbors.push([x + i, y + j]);
+      count++;
+    }
+  }
+  return neighbors;
+}
+
 const testCases = [
   {
     testDescription: "Game starts empty by default",
@@ -43,6 +58,19 @@ const testCases = [
       const deadWorld = new GameOfLife();
       deadWorld.step();
       assert(deadWorld.liveCells.length === 0);
+    },
+  },
+
+  {
+    testDescription: "Dead cell without three neighbors stays dead",
+    testFun: () => {
+      const cellPos = [0, 0];
+      for (let numNeighbors of [0, 1, 2, 4, 5, 6, 7, 8]) {
+        let neighborPositions = generateNeighbors(cellPos, numNeighbors);
+        let game = new GameOfLife(neighborPositions);
+        game.step();
+        assert(!game.cellIsAlive(...cellPos));
+      }
     },
   },
 ];
