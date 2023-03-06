e14465f835ef0b163aa789da2f77090724d9dc92
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2023-03-06 13:28:40 -0300

    Allow user to add/remove cells

--- index.js
+++ index.js
@@ -20,6 +20,15 @@ slider.addEventListener("input", function () {
   framesPerSecond = this.value;
 });
 
+canvas.addEventListener("click", function (event) {
+  if (!isPaused) return;
+  const x = event.clientX - canvas.offsetLeft;
+  const y = event.clientY - canvas.offsetTop;
+  const xGridCoord = Math.floor(x / cellSize);
+  const yGridCoord = Math.floor(y / cellSize);
+  game.toggleCell(xGridCoord, yGridCoord);
+});
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   for (let cell of game.liveCells) {
